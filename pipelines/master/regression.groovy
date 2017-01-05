e7b619076f9642c9261f90f9794ba6404da94086
Richard Berger <richard.berger@temple.edu>
2017-01-05 18:34:26 +0100

    Fix path of scripts and examples

--- pipelines/master/regression.groovy
+++ pipelines/master/regression.groovy
@@ -135,7 +135,7 @@ node {
                 sh 'make -j 8 -C src mode=shexe ${MACH} CC="${COMP}" LINK="${COMP}" LMP_INC="${LMP_INC}" JPG_LIB="${JPG_LIB}"'
 
                 stage 'Testing'
-                sh 'python2 lammps_testing/regression.py 8 "mpiexec -np 8 ${LAMMPS_BINARY} -v CORES 8" tests/examples -exclude kim 2>&1 |tee test.out'
+                sh 'python2 lammps-testing/lammps_testing/regression.py 8 "mpiexec -np 8 ${LAMMPS_BINARY} -v CORES 8" lammps-testing/tests/examples -exclude kim 2>&1 |tee test.out'
                 sh 'grep "*** no failures ***" test.out'
                 /*
                 sh '''
