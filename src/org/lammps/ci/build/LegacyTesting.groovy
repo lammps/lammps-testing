ef8937f8971bc1c20d242f8a6aecffb4db33522a
Richard Berger <richard.berger@temple.edu>
2019-01-05 23:38:06 +0100

    Revert "Use nose2"
    This reverts commit b3de88d41245eeef48fd6cd0ac12427c87a4ee08.

--- src/org/lammps/ci/build/LegacyTesting.groovy
+++ src/org/lammps/ci/build/LegacyTesting.groovy
@@ -104,7 +104,7 @@ abstract class LegacyTesting implements Serializable {
         steps.sh '''
         virtualenv pyenv
         source pyenv/bin/activate
-        pip install nose2
+        pip install nose
         deactivate
         '''
 
