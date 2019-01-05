35d6a327b629459816e1845045e9cb53c87032c0
Richard Berger <richard.berger@temple.edu>
2019-01-05 10:28:14 +0100

    Fix clang compilation

--- src/org/lammps/ci/build/SerialClang.groovy
+++ src/org/lammps/ci/build/SerialClang.groovy
@@ -4,6 +4,7 @@ class SerialClang extends Serial {
     SerialClang(steps) {
         super(steps)
         name = 'jenkins/serial-clang'
+        compiler = 'clang++'
         c_compiler = 'clang'
         cxx_compiler = 'clang++'
     }
--- src/org/lammps/ci/build/ShlibClang.groovy
+++ src/org/lammps/ci/build/ShlibClang.groovy
@@ -4,6 +4,7 @@ class ShlibClang extends Shlib {
     ShlibClang(steps) {
         super(steps)
         name = 'jenkins/shlib-clang'
+        compiler = 'clang++'
         c_compiler = 'clang'
         cxx_compiler = 'clang++'
     }
