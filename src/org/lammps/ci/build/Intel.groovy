87798e1bf4cc4589093cdfa1522d533b94de8160
Richard Berger <richard.berger@temple.edu>
2019-01-06 18:30:40 +0100

    Fixup compiler warnings and enable ccache for Intel compiler

--- src/org/lammps/ci/build/Intel.groovy
+++ src/org/lammps/ci/build/Intel.groovy
@@ -4,8 +4,8 @@ class Intel extends LegacyBuild {
     Intel(steps) {
         super('jenkins/intel', steps)
         compiler    = 'mpiicpc -qopenmp'
-        c_compiler  = 'icc'
-        cxx_compiler  = 'icpc'
+        c_compiler  = 'ccache icc'
+        cxx_compiler  = 'ccache icpc'
         lammps_mode = LAMMPS_MODE.shexe
         lammps_mach = 'mpi'
         lammps_size = LAMMPS_SIZES.SMALLBIG
@@ -19,5 +19,9 @@ class Intel extends LegacyBuild {
        super.configure()
        steps.env.LMP_INC="-g -mkl=sequential -DLAMMPS_MEMALIGN=64 -qno-offload -fno-alias -ansi-alias -restrict -xHost -O2 -fp-model fast=2 -no-prec-div -qoverride-limits -DFFT_MKL -DFFT_DOUBLE -DLAMMPS_GZIP -DLAMMPS_PNG -DLAMMPS_JPEG -DLAMMPS_${lammps_size} -Wall -w2"
        steps.env.JPG_LIB='-g -ltbbmalloc -ltbbmalloc_proxy -ljpeg -lpng -lz -mkl=sequential'
+    }
+
+    def post_actions() {
+        steps.warnings consoleParsers: [[parserName: 'Intel C Compiler']]
     }
 }
