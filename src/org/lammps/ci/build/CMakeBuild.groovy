3b54a0370fa764fc50a5419e0d0b4962f18312a5
Richard Berger <richard.berger@temple.edu>
2019-01-06 19:50:44 +0100

    Revert "Add color to cmake build"
    This reverts commit 5d320003515f746216b159d5e57dee36c785592c.

--- src/org/lammps/ci/build/CMakeBuild.groovy
+++ src/org/lammps/ci/build/CMakeBuild.groovy
@@ -32,11 +32,9 @@ class CMakeBuild implements Serializable {
     def build() {
 
         steps.stage('Compiling') {
-            steps.ansiColor('xterm') {
-                steps.sh '''#!/bin/bash -l
-                make -C build -j 8
-                '''
-            }
+            steps.sh '''#!/bin/bash -l
+            make -C build -j 8
+            '''
         }
 
         steps.sh 'ccache -s'
