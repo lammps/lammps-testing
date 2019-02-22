f5580794e13d38d3c1c25d6e1d8041f4f9cf98a6
Richard Berger <richard.berger@temple.edu>
2019-02-22 03:34:57 -0500

    Revert "Control GH status updates via environment variable"
    This reverts commit 1552af692738fc1865f7c8e83f29853e0e1c9933.

--- src/org/lammps/ci/Utils.groovy
+++ src/org/lammps/ci/Utils.groovy
@@ -1,9 +1,7 @@
 package org.lammps.ci
 
 def setGitHubCommitStatus(project_url, build_name, git_commit, message, state) {
-    if (env.LAMMPS_REPORT_GH_STATUS.toBoolean()) {
-        step([$class: 'GitHubCommitStatusSetter', commitShaSource: [$class: 'ManuallyEnteredShaSource', sha: git_commit], contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: build_name], reposSource: [$class: 'ManuallyEnteredRepositorySource', url: project_url], statusResultSource: [$class: 'ConditionalStatusResultSource', results: [[$class: 'AnyBuildResult', message: message, state: state]]]])
-    }
+    step([$class: 'GitHubCommitStatusSetter', commitShaSource: [$class: 'ManuallyEnteredShaSource', sha: git_commit], contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: build_name], reposSource: [$class: 'ManuallyEnteredRepositorySource', url: project_url], statusResultSource: [$class: 'ConditionalStatusResultSource', results: [[$class: 'AnyBuildResult', message: message, state: state]]]])
 }
 
 return this
