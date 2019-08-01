21f890475f01c64eb4992a37cab3e4a7bfd92ad2
Axel Kohlmeyer <akohlmey@gmail.com>
2019-08-01 08:11:48 -0400

    enable exceptions for OpenMPI+Clang build

--- src/org/lammps/ci/build/OpenMPIClang.groovy
+++ src/org/lammps/ci/build/OpenMPIClang.groovy
@@ -4,6 +4,7 @@ class OpenMPIClang extends OpenMPI {
     OpenMPIClang(steps) {
         super(steps)
         name = 'jenkins/openmpi-clang'
+        lammps_except = '-DLAMMPS_EXCEPTIONS'
         c_compiler = 'clang'
         cxx_compiler = 'clang++'
     }
