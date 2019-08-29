5e662f55f2fc8fe0a1892fefaba0d7756417cf8e
Richard Berger <richard.berger@temple.edu>
2019-08-29 13:38:19 -0400

    Activate reporting for Docker builds on master

--- pipelines/master/docker/centos7_openmpi_py3.groovy
+++ pipelines/master/docker/centos7_openmpi_py3.groovy
@@ -5,5 +5,5 @@ node('atlas2') {
     def build = new LAMMPSBuild()
     def docker_image_name = "lammps/lammps:latest_centos7_openmpi_py3"
     def dockerfile = "lammps-packages/docker/centos/7/py3/Dockerfile"
-    build.container_build(env.JOB_BASE_NAME, docker_image_name, dockerfile, false, false)
+    build.container_build(env.JOB_BASE_NAME, docker_image_name, dockerfile, true, true)
 }
