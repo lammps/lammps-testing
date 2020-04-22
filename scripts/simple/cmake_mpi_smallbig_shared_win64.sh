f4bc7f592bde50addfe4b9a33b875b81b84fe6a3
Richard Berger <richard.berger@temple.edu>
2020-04-22 17:39:58 -0400

    Add mingw build scripts for win32 and win64

---
+++ scripts/simple/cmake_mpi_smallbig_shared_win64.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+CMAKE_COMMAND=mingw64-cmake
+
+LAMMPS_COMPILE_NPROC=8
+LAMMPS_CXX_COMPILER_FLAGS="-Wall -Wno-maybe-uninitialized"
+LAMMPS_C_COMPILER_FLAGS="-Wall -Wno-maybe-uninitialized"
+
+export CCACHE_DIR="$PWD/.ccache"
+
+# Set up environment
+ccache -M 5G
+
+# Configure
+${CMAKE_COMMAND} -C ${LAMMPS_DIR}/cmake/presets/mingw-cross.cmake \
+      -D CXX_COMPILER_LAUNCHER=ccache \
+      -D CMAKE_CXX_FLAGS="${LAMMPS_CXX_COMPILER_FLAGS}" \
+      -D CMAKE_C_FLAGS="${LAMMPS_C_COMPILER_FLAGS}" \
+      -D BUILD_MPI=on \
+      -D BUILD_OMP=on \
+      -D BUILD_SHARED_LIBS=on \
+      -D LAMMPS_SIZES=SMALLBIG \
+      -D LAMMPS_EXCEPTIONS=on \
+      ${LAMMPS_DIR}/cmake || exit 1
+
+# Build
+make -j ${LAMMPS_COMPILE_NPROC} || exit 1
+
+ccache -s
---
+++ scripts/simple/cmake_serial_smallsmall_static_win32.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+CMAKE_COMMAND=mingw32-cmake
+
+LAMMPS_COMPILE_NPROC=8
+LAMMPS_CXX_COMPILER_FLAGS="-Wall -Wno-maybe-uninitialized"
+LAMMPS_C_COMPILER_FLAGS="-Wall -Wno-maybe-uninitialized"
+
+export CCACHE_DIR="$PWD/.ccache"
+
+# Set up environment
+ccache -M 5G
+
+# Configure
+${CMAKE_COMMAND} -C ${LAMMPS_DIR}/cmake/presets/mingw-cross.cmake \
+      -D CMAKE_CXX_FLAGS="${LAMMPS_CXX_COMPILER_FLAGS}" \
+      -D CMAKE_C_FLAGS="${LAMMPS_C_COMPILER_FLAGS}" \
+      -D BUILD_MPI=off \
+      -D BUILD_OMP=on \
+      -D BUILD_SHARED_LIBS=off \
+      -D LAMMPS_SIZES=SMALLSMALL \
+      -D LAMMPS_EXCEPTIONS=off \
+      ${LAMMPS_DIR}/cmake || exit 1
+
+# Build
+make -j ${LAMMPS_COMPILE_NPROC} || exit 1
+
+ccache -s
