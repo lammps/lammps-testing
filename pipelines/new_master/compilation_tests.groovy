ca77de7090338ff27cf0f08a67f2ac59f9caec11
Richard Berger <richard.berger@temple.edu>
2020-04-23 19:15:04 -0400

    Make compilation_tests use YAML file names

--- pipelines/new_master/compilation_tests.groovy
+++ pipelines/new_master/compilation_tests.groovy
@@ -2,25 +2,23 @@ node('atlas2') {
     stage('Checkout') {
         dir('lammps') {
             commit = checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', depth: 1, noTags: false, reference: '', shallow: true]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'lammps-jenkins', url: 'https://github.com/lammps/lammps']]])
-            env.GIT_COMMIT = commit
         }
         
         dir('lammps-testing') {
             checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/lammps_test']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'lammps-jenkins', url: 'https://github.com/lammps/lammps-testing']]]
         }
     }
+
+    def yaml_files = findFiles glob: 'lammps-testing/scripts/simple/*.yml'
+    def configurations = yaml_files.collect { yaml_file -> yaml_file.name.take(yaml_file.name.lastIndexOf('.'))  }
+
+    def jobs = configurations.collectEntries { container ->
+        ["${container}": {
+            build job: "${container}_compilation_tests", parameters: [string(name: 'GIT_COMMIT', value: commit.GIT_COMMIT), string(name: 'WORKSPACE_PARENT', value: env.WORKSPACE)]
+        }]
+    }
     
     stage('Build') {
-        parallel(
-            "ubuntu": {
-                build job: 'ubuntu_compilation_tests', parameters: [string(name: 'GIT_COMMIT', value: commit.GIT_COMMIT), string(name: 'WORKSPACE_PARENT', value: env.WORKSPACE)]
-            },
-            "centos7": {
-                build job: 'centos7_compilation_tests', parameters: [string(name: 'GIT_COMMIT', value: commit.GIT_COMMIT), string(name: 'WORKSPACE_PARENT', value: env.WORKSPACE)]
-            },
-            "windows": {
-                build job: 'windows_compilation_tests', parameters: [string(name: 'GIT_COMMIT', value: commit.GIT_COMMIT), string(name: 'WORKSPACE_PARENT', value: env.WORKSPACE)]
-            }
-        )
+        parallel jobs
     }
 }
