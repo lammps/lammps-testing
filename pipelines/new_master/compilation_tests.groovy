811067f901596f7608d64063aabc1d713982fb57
Richard Berger <richard.berger@temple.edu>
2020-04-24 12:42:00 -0400

    Update compilation_tests.groovy

--- pipelines/new_master/compilation_tests.groovy
+++ pipelines/new_master/compilation_tests.groovy
@@ -19,19 +19,19 @@ node('atlas2') {
     def configurations = yaml_files.collectEntries { yaml_file -> get_configuration(yaml_file)  }
 
     configurations.each { container, config ->
+        def jobs = config.builds.collectEntries { build ->
+            ["${build}": {
+                echo "Building ${container}/${build}"
+                build job: "${container}/${build}",
+                    parameters: [
+                        string(name: 'GIT_COMMIT', value: commit.GIT_COMMIT),
+                        string(name: 'WORKSPACE_PARENT', value: env.WORKSPACE),
+                        ]
+            }]
+        }
+
         stage(container) {
             echo "Running ${config.display_name}"
-
-            def jobs = config.builds.collectEntries { build ->
-                ["${build}": {
-                    build job: "${container}/${build}",
-                        parameters: [
-                            string(name: 'GIT_COMMIT', value: commit.GIT_COMMIT),
-                            string(name: 'WORKSPACE_PARENT', value: env.WORKSPACE),
-                            ]
-                }]
-            }
-
             parallel jobs
         }
     }
