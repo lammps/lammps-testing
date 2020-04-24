8bf4dc7581701f1d4908b1559bfd7a1fefd58f18
Richard Berger <richard.berger@temple.edu>
2020-04-24 19:13:12 -0400

    Update outdated pipelines

--- pipelines/build-envs/ubuntu_18.04.groovy
+++ pipelines/build-envs/ubuntu_18.04.groovy
@@ -3,18 +3,18 @@ node {
     def version = '18.04'
     
     stage('Checkout') {
-        checkout([$class: 'GitSCM', userRemoteConfigs: [[url: 'https://github.com/lammps/lammps-testing.git', credentialsId: 'lammps-jenkins']],
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/lammps/lammps-testing.git', credentialsId: 'lammps-jenkins']],
                   extensions: [[$class: 'PathRestriction', includedRegions: 'envs/'+os+'/'+version+'/.*']]
                  ])
     }
 
    
-    docker.withRegistry('https://registry.hub.docker.com', 'docker-registry-login') {
+    docker.withRegistry('http://glados2.cst.temple.edu:5000') {
         dir('envs/' + os + '/' + version + '/') {
-            def image_name = 'rbberger/lammps-testing:' + os + '_' + version
+            def image_name = 'lammps_testing:' + os + '_' + version
             
             stage 'Build'
-            docker.build(image_name)
+            docker.build(image_name, '--pull=true --no-cache=true .')
             
             stage 'Publish'
             def image = docker.image(image_name)
