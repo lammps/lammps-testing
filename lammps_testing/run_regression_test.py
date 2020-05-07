c5a9578467a3e6835f913b4475a02cb07006d11b
Richard Berger <richard.berger@temple.edu>
2020-05-07 17:42:17 -0400

    Allow customization of lammps_run_regression_test

--- lammps_testing/run_regression_test.py
+++ lammps_testing/run_regression_test.py
@@ -2,7 +2,7 @@
 import os
 import sys
 import argparse
-from .common import Settings, MPIRunner
+from .common import Settings, LocalRunner, MPIRunner
 from .regression_testing import RegressionTest
 
 def regression(args, settings):
@@ -12,10 +12,14 @@ def regression(args, settings):
     assert(input_script.startswith("in."))
     name = input_script[3:]
 
-    runner = MPIRunner(args.binary, nprocs=8)
+    if args.nprocs > 1:
+        runner = LocalRunner(args.binary)
+    else:
+        runner = MPIRunner(args.binary, nprocs=args.nprocs)
+    
     runner.working_dir = test_directory
 
-    test = RegressionTest(name, test_directory, descriptor="8", options=['-v', 'CORES', 8])
+    test = RegressionTest(name, test_directory, descriptor=args.descriptor, options=args.lammps_options.split())
     result = test.verify(runner, norm=args.norm, tolerance=args.tolerance, verbose=args.verbose, generate_plots=args.generate_plots)
 
     if args.output_attachements:
@@ -39,6 +43,9 @@ def main():
     # create the top-level parser
     parser = argparse.ArgumentParser(prog='run_regression')
     parser.add_argument('--tolerance', default=1e-6, help='')
+    parser.add_argument('--nprocs', default=8)
+    parser.add_argument('--descriptor', type=str, default="8")
+    parser.add_argument('--lammps-options', type=str, default="-v CORES 8")
     parser.add_argument('--norm', choices=('L1', 'L2', 'max'), default='max', help='')
     parser.add_argument('-v', '--verbose', action='store_true', help='Output thermo quantities that do not match')
     parser.add_argument('-g', '--generate-plots', action='store_true', help='Generate plots of thermo quantities that do not match')
