34accba972fc862fe1fefe55885ac6936fc0d98c
Richard Berger <richard.berger@temple.edu>
2020-05-07 20:03:52 -0400

    Change locale to C.UTF-8

--- scripts/simple/regression_tests/cmake_regression_mpi/test.sh
+++ scripts/simple/regression_tests/cmake_regression_mpi/test.sh
@@ -10,6 +10,7 @@ source pyenv/bin/activate
 export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib64:$VIRTUAL_ENV/lib:$LD_LIBRARY_PATH
 export LAMMPS_BINARY=${VIRTUAL_ENV}/bin/lmp
 export LAMMPS_POTENTIALS="${VIRTUAL_ENV}/share/lammps/potentials"
+export LC_ALL=C.UTF-8
 
 rm *.out *.xml || true
 
