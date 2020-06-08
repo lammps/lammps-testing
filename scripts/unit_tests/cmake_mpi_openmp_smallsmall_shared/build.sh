7335cb7ffb55c9d6e8df84b51da9f6519cac27e0
Richard Berger <richard.berger@temple.edu>
2020-06-08 11:15:47 -0400

    Add SMALLSMALL and BIGBIG variants to unit tests

---
+++ scripts/unit_tests/cmake_mpi_openmp_smallsmall_shared/build.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -x
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+SCRIPT_BASE_DIR=$LAMMPS_TESTING_DIR/scripts
+
+if [ -z "${LAMMPS_DIR}" ]
+then
+    echo "Must set LAMMPS_DIR environment variable"
+    exit 1
+fi
+
+if [ -z "${LAMMPS_CI_RUNNER}" ]
+then
+    # local testing
+    BUILD=build-$(basename $0 .sh)
+else
+    # when running lammps_test or inside jenkins
+    BUILD=build
+fi
+
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+
+if exists "cmake3"
+then
+    CMAKE_COMMAND=cmake3
+else
+    CMAKE_COMMAND=cmake
+fi
+
+LAMMPS_COMPILE_NPROC=${LAMMPS_COMPILE_NPROC-8}
+
+export CCACHE_DIR="$PWD/.ccache"
+export PYTHON=$(which python3)
+
+# Set up environment
+ccache -M 5G
+
+$SCRIPT_BASE_DIR/common/init_testing_venv.sh
+
+source pyenv/bin/activate
+
+# Create build directory
+if [ -d "${BUILD}" ]; then
+    rm -rf ${BUILD}
+fi
+
+mkdir -p ${BUILD}
+cd ${BUILD}
+
+# Configure
+${CMAKE_COMMAND} \
+      -C ${LAMMPS_DIR}/cmake/presets/minimal.cmake \
+      -D CMAKE_BUILD_TYPE="RelWithDebug" \
+      -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
+      -D CMAKE_TUNE_FLAGS="-Wall -Wextra -Wno-unused-result" \
+      -D CMAKE_INSTALL_PREFIX=${VIRTUAL_ENV} \
+      -D BUILD_MPI=on \
+      -D BUILD_OMP=on \
+      -D PKG_USER-OMP=on \
+      -D BUILD_SHARED_LIBS=on \
+      -D LAMMPS_SIZES=SMALLSMALL \
+      -D LAMMPS_EXCEPTIONS=on \
+      -D ENABLE_TESTING=on \
+      -D ENABLE_COVERAGE=on \
+      ${LAMMPS_DIR}/cmake || exit 1
+
+# Build
+cmake --build . -- -j ${LAMMPS_COMPILE_NPROC} || exit 1
+
+ccache -s
