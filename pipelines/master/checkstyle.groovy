8a641933a74899a8d8d46e8da6778e1501343043
Richard Berger <richard.berger@temple.edu>
2020-06-19 20:24:15 -0400

    Update checkstyle job

--- pipelines/master/checkstyle.groovy
+++ pipelines/master/checkstyle.groovy
@@ -8,6 +8,10 @@ def lammps_branch = 'master'
 
 node('atlas2'){
     def utils = new Utils()
+    env.LAMMPS_CONTAINER_DIR = "/home/jenkins/containers"
+    def container = "fedora32_mingw"
+    def container_args = ""
+    def launch_container = "singularity exec ${container_args} \$LAMMPS_CONTAINER_DIR/${container}.sif"
 
     stage('Checkout') {
         dir('lammps') {
@@ -19,24 +23,45 @@ node('atlas2'){
         utils.setGitHubCommitStatus(project_url, env.JOB_NAME, commit.GIT_COMMIT, 'building...', 'PENDING')
     }
 
-    stage('Whitespace') {
-        sh 'egrep -Rn "\\s+$"  lammps/src/ > whitespace.log || true'
-        recordIssues qualityGates: [[threshold: 1, type: 'TOTAL', unstable: false]], filters: [includeFile('.*\\.cpp'), includeFile('.*\\.h'), excludeFile('lammps/src/USER-MGPT/*')], tools: [groovyScript(parserId: 'whitespace', pattern: 'whitespace.log')]
-    }
+    try {
+        stage('Whitespace') {
+            dir('lammps') {
+                if(fileExists('tools/coding_standard/whitespace.py')) {
+                    sh(label: "Check for whitespace errors",
+                       script: "${launch_container} python3 tools/coding_standard/whitespace.py .")
+                } else {
+                    echo 'Skipping'
+                }
+            }
+        }
 
-    if (currentBuild.result == 'FAILURE') {
-        if (set_github_status) {
-            utils.setGitHubCommitStatus(project_url, env.JOB_NAME, commit.GIT_COMMIT, 'Style issues detected!', 'FAILURE')
+        stage('File Permissions') {
+            dir('lammps') {
+                if(fileExists('tools/coding_standard/permissions.py')) {
+                    sh(label: "Check file permissions",
+                       script: "${launch_container} python3 tools/coding_standard/permissions.py .")
+                } else {
+                    echo 'Skipping'
+                }
+            }
         }
-        if (send_slack) {
-            slackSend color: 'bad', message: "Build <${env.BUILD_URL}|#${env.BUILD_NUMBER}> of ${env.JOB_NAME} failed!"
-        }
-    } else {
-        if (set_github_status) {
-            utils.setGitHubCommitStatus(project_url, env.JOB_NAME, commit.GIT_COMMIT, 'No warnings', 'SUCCESS')
-        }
-        if (send_slack) {
-            slackSend color: 'good', message: "Build <${env.BUILD_URL}|#${env.BUILD_NUMBER}> of ${env.JOB_NAME} succeeded!"
+    } catch(Exception e) {
+        currentBuild.result = 'FAILURE'
+    } finally {
+        if (currentBuild.result == 'FAILURE') {
+            if (set_github_status) {
+                utils.setGitHubCommitStatus(project_url, env.JOB_NAME, commit.GIT_COMMIT, 'Style issues detected!', 'FAILURE')
+            }
+            if (send_slack) {
+                slackSend color: 'bad', message: "Build <${env.BUILD_URL}|#${env.BUILD_NUMBER}> of ${env.JOB_NAME} failed!"
+            }
+        } else {
+            if (set_github_status) {
+                utils.setGitHubCommitStatus(project_url, env.JOB_NAME, commit.GIT_COMMIT, 'No warnings', 'SUCCESS')
+            }
+            if (send_slack) {
+                slackSend color: 'good', message: "Build <${env.BUILD_URL}|#${env.BUILD_NUMBER}> of ${env.JOB_NAME} succeeded!"
+            }
         }
     }
 }
