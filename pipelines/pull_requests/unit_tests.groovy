64ebafa721b21b25c2ad4c058d937ca80c963c73
Richard Berger <richard.berger@temple.edu>
2020-07-30 11:26:05 -0400

    Set PR unit test status

--- pipelines/pull_requests/unit_tests.groovy
+++ pipelines/pull_requests/unit_tests.groovy
@@ -2,7 +2,7 @@
 import org.lammps.ci.Utils
 
 def project_url = 'https://github.com/lammps/lammps.git'
-def set_github_status = false
+def set_github_status = true
 def send_slack = true
 
 def lammps_testing_branch = "master"
