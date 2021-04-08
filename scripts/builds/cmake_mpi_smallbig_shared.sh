ddc10a8f3ddb9c78e3e68beb78743c4e44ff83c0
Richard Berger <richard.berger@temple.edu>
2021-04-08 16:23:20 -0400

    Add offline build variant

--- scripts/builds/cmake_mpi_smallbig_shared.sh
+++ scripts/builds/cmake_mpi_smallbig_shared.sh
@@ -33,6 +33,13 @@ then
     export CCACHE_DIR="$PWD/.ccache"
 fi
 
+if [ -z "${HTTP_CACHE_URL}" || -z "${LAMMPS_HTTP_CACHE_CONFIG}" ]
+then
+    BUILD_HTTP_CACHE_CONFIGURATION=""
+else
+    BUILD_HTTP_CACHE_CONFIGURATION="-D LAMMPS_DOWNLOADS_URL=${HTTP_CACHE_URL} -C ${LAMMPS_HTTP_CACHE_CONFIG}"
+fi
+
 export PYTHON=$(which python3)
 
 # Set up environment
@@ -50,7 +57,9 @@ mkdir -p ${BUILD}
 cd ${BUILD}
 
 # Configure
-${CMAKE_COMMAND} -C ${LAMMPS_DIR}/cmake/presets/most.cmake \
+${CMAKE_COMMAND} \
+      ${BUILD_HTTP_CACHE_CONFIGURATION} \
+      -C ${LAMMPS_DIR}/cmake/presets/most.cmake \
       -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
       -D CMAKE_TUNE_FLAGS="-Wall -Wextra -Wno-unused-result -Wno-maybe-uninitialized" \
       -D CMAKE_INSTALL_PREFIX=${VIRTUAL_ENV} \
