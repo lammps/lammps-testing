5417f415442c815ffae484c7a090dd7a7c3bb031
Richard Berger <richard.berger@temple.edu>
2021-05-15 10:21:06 -0400

    Fixup build_jobs pipeline

--- pipelines/master/build_docs.groovy
+++ pipelines/master/build_docs.groovy
@@ -16,7 +16,7 @@ node('slow') {
 
     stage('Checkout') {
         dir('lammps') {
-            commit = checkout([$class: 'GitSCM', branches: [[name: "*/${lammps_branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', depth: 1, noTags: false, reference: '', shallow: true]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'lammps-jenkins', url: project_url]]])
+            commit = checkout([$class: 'GitSCM', branches: [[name: "*/${lammps_branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', depth: 2, noTags: false, reference: '', shallow: true]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'lammps-jenkins', url: project_url]]])
         }
     }
 
@@ -59,7 +59,7 @@ node('slow') {
 
         stage('Publish') {
             sshPublisher(publishers: [sshPublisherDesc(configName: 'docs.lammps.org', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'rm -rf /var/www/lammps/docs/*', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: ''), sshTransfer(cleanRemote: false, excludes: '', execCommand: 'tar xvzf /var/www/lammps/docs/lammps-docs.tar.gz -C /var/www/lammps/docs && rm /var/www/lammps/docs/lammps-docs.tar.gz', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: 'lammps/docs', remoteDirectorySDF: false, removePrefix: 'lammps/doc/', sourceFiles: 'lammps/doc/lammps-docs.tar.gz')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
-          sshPublisher(publishers: [sshPublisherDesc(configName: 'docs.lammps.org', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '/var/www/lammps/docs/', remoteDirectorySDF: false, removePrefix: 'lammps/doc/', sourceFiles: 'lammps/doc/Manual.pdf')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
+            sshPublisher(publishers: [sshPublisherDesc(configName: 'docs.lammps.org', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: 'lammps/docs/', remoteDirectorySDF: false, removePrefix: 'lammps/doc/', sourceFiles: 'lammps/doc/Manual.pdf')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
         }
     } catch (caughtErr) {
         err = caughtErr
