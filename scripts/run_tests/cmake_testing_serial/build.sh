8dc370def19a9860c133fb85e0c0758d4a5c1453
Axel Kohlmeyer <akohlmey@gmail.com>
2021-07-06 18:56:25 -0400

    update run and regression tests build scripts for package rename

--- scripts/run_tests/cmake_testing_serial/build.sh
+++ scripts/run_tests/cmake_testing_serial/build.sh
@@ -64,7 +64,7 @@ cd ${BUILD}
 # Configure
 ${CMAKE_COMMAND} \
       ${BUILD_HTTP_CACHE_CONFIGURATION} \
-      -C ${LAMMPS_DIR}/cmake/presets/all_off.cmake \
+      -C ${LAMMPS_DIR}/cmake/presets/most.cmake \
       -D CMAKE_BUILD_TYPE="RelWithDebug" \
       -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
       -D CMAKE_TUNE_FLAGS="-O3 -Wall -Wextra -Wno-unused-result -Wno-unused-parameter -Wno-maybe-uninitialized" \
@@ -75,60 +75,18 @@ ${CMAKE_COMMAND} \
       -D BUILD_SHARED_LIBS=on \
       -D LAMMPS_SIZES=SMALLSMALL \
       -D LAMMPS_EXCEPTIONS=on \
-      -D PKG_ASPHERE=on \
-      -D PKG_BODY=on \
-      -D PKG_CLASS2=on \
-      -D PKG_COLLOID=on \
-      -D PKG_COMPRESS=on \
-      -D PKG_CORESHELL=on \
-      -D PKG_DIPOLE=on \
-      -D PKG_GRANULAR=on \
-      -D PKG_KSPACE=on \
-      -D PKG_MANYBODY=on \
-      -D PKG_MC=on \
-      -D PKG_MISC=on \
-      -D PKG_MOLECULE=on \
-      -D PKG_OPT=on \
-      -D PKG_PERI=on \
-      -D PKG_POEMS=on \
-      -D PKG_PYTHON=on \
-      -D PKG_QEQ=on \
-      -D PKG_REPLICA=on \
-      -D PKG_RIGID=on \
-      -D PKG_SHOCK=on \
-      -D PKG_SNAP=on \
-      -D PKG_SPIN=on \
-      -D PKG_SRD=on \
-      -D PKG_USER-AWPMD=on \
-      -D PKG_USER-BOCS=on \
-      -D PKG_USER-CGDNA=on \
-      -D PKG_USER-CGSDK=on \
-      -D PKG_USER-COLVARS=on \
-      -D PKG_USER-DIFFRACTION=on \
-      -D PKG_USER-DPD=on \
-      -D PKG_USER-DRUDE=on \
-      -D PKG_USER-EFF=on \
-      -D PKG_USER-FEP=on \
-      -D PKG_USER-H5MD=on \
-      -D PKG_USER-MANIFOLD=on \
-      -D PKG_USER-MEAMC=on \
-      -D PKG_USER-MESODPD=on \
-      -D PKG_USER-MGPT=on \
-      -D PKG_USER-MISC=on \
-      -D PKG_USER-MOFFF=on \
-      -D PKG_USER-MOLFILE=on \
-      -D PKG_USER-PHONON=on \
-      -D PKG_USER-PTM=on \
-      -D PKG_USER-QTB=on \
-      -D PKG_USER-REAXC=on \
-      -D PKG_USER-SDPD=on \
-      -D PKG_USER-SMTBQ=on \
-      -D PKG_USER-SPH=on \
-      -D PKG_USER-TALLY=on \
-      -D PKG_USER-UEF=on \
-      -D PKG_USER-YAFF=on \
-      -D PKG_VORONOI=on \
-      -D PKG_USER-ATC=on \
+      -D PKG_ATC=on \
+      -D PKG_AWPMD=on \
+      -D PKG_H5MD=on \
+      -D PKG_LATBOLTZ=on \
+      -D PKG_MANIFOLD=on \
+      -D PKG_MGPT=on \
+      -D PKG_MOLFILE=on \
+      -D PKG_MPIIO=on \
+      -D PKG_PTM=on \
+      -D PKG_QTB=on \
+      -D PKG_SMTBQ=on \
+      -D PKG_TALLY=on \
       ${LAMMPS_DIR}/cmake || exit 1
 
 # Build
