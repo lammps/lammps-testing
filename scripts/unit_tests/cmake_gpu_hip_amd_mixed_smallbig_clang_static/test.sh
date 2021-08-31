bd9fca3eed929dfea572eb10cefc52e051304de4
Richard Berger <richard.berger@temple.edu>
2021-08-31 18:06:20 -0400

    Add tests for HIP

---
+++ scripts/unit_tests/cmake_gpu_hip_amd_mixed_smallbig_clang_static/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export WORKING_DIR=$PWD
+SCRIPTDIR="$(dirname "$(realpath "$0")")"
+
+if [ -z "${LAMMPS_CI_RUNNER}" ]
+then
+    # local testing
+    BUILD=build-$(basename $0 .sh)
+else
+    # when running lammps_test or inside jenkins
+    BUILD=build
+fi
+
+source pyenv/bin/activate
+export LC_ALL=C.UTF-8
+
+cd ${BUILD}
+
+echo "Running tests in parallel with CTEST_PARALLEL_LEVEL=$CTEST_PARALLEL_LEVEL"
+ctest -V --no-compress-output -T Test
+
+make gen_coverage_xml
+
+deactivate
