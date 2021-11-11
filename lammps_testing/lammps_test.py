f586a3fd78e217fd88330154cf2d128f36f1174e
Richard Berger <richard.berger@temple.edu>
2021-11-11 16:52:15 -0500

    Remove old code

--- lammps_testing/lammps_test.py
+++ lammps_testing/lammps_test.py
@@ -3,11 +3,9 @@ import argparse
 import glob
 import os
 import re
-import subprocess
 import sys
 
-from termcolor import colored
-from .common import logger, Settings, Container, get_containers, get_configurations, get_container, get_configuration
+from .common import logger, Settings, get_containers, get_configurations, get_container
 from .tests import RunTest, RegressionTest, UnitTest
 
 from .commands.env import init_command as init_env_command
@@ -15,102 +13,6 @@ from .commands.config import init_command as init_config_command
 from .commands.build import init_command as init_build_command
 from .commands.compile import init_command as init_compile_command
 from .commands.reg import init_command as init_reg_command
-
-class TestRunner():
-    def __init__(self, builder, settings):
-        self.builder = builder
-        self.container = builder.container
-        self.settings = settings
-
-    @property
-    def name(self):
-        return self.builder.name + "_testing"
-
-    @property
-    def working_dir(self):
-        return os.path.join(self.settings.cache_dir, self.name)
-
-    @property
-    def lammps_build_dir(self):
-        return self.builder.working_dir
-
-    def run(self):
-        if not self.container.exists:
-            logger.error(f"Container '{self.container.name}' is missing! Build with 'lammps_test build_container {self.container.name}' first!")
-            sys.exit(-1)
-        if not self.builder.exists:
-            logger.error(f"LAMMPS build '{self.builder.name}' is missing! Build with 'lammps_test build' first!")
-            sys.exit(-1)
-
-        logger.info(f"LAMMPS Build: {self.builder.working_dir}")
-        logger.info(f"Workdir: {self.working_dir}")
-        os.makedirs(self.working_dir, exist_ok=True)
-        build_env = os.environ.copy()
-        build_env["LAMMPS_BUILD_DIR"] = self.lammps_build_dir
-        build_env["LAMMPS_TESTS"] = "tests/test_commands.py tests/test_examples.py"
-        build_env["LAMMPS_TESTING_NPROC"] = "1"
-        build_env["LAMMPS_TEST_MODES"] = "omp"
-        scripts_dir = os.path.join(self.settings.lammps_testing_dir, 'scripts')
-        run_tests_script = os.path.join(scripts_dir, 'RunTests.sh')
-        subprocess.call(['singularity', 'run', '-B', f'{self.settings.lammps_dir}/:{self.settings.lammps_dir}/', '-B', f'{scripts_dir}/:{scripts_dir}/', self.container.container, run_tests_script], env=build_env, cwd=self.working_dir)
-
-class LocalRunner(object):
-    def __init__(self, builder, settings):
-        self.builder = builder
-        self.container = builder.container
-        self.settings = settings
-        self.working_dir = os.getcwd()
-
-    @property
-    def lammps_build_dir(self):
-        return self.builder.working_dir
-
-    def run_command(self, command, env, cwd, stdout=None):
-        if stdout:
-            subprocess.call(command, env=env, cwd=self.working_dir, stdout=stdout, stderr=subprocess.STDOUT)
-        else:
-            subprocess.call(command, env=env, cwd=self.working_dir)
-
-    @property
-    def build_env(self):
-        env = os.environ.copy()
-        env["LAMMPS_BUILD_DIR"] = self.lammps_build_dir
-        return env
-
-    def run(self, args, stdout=None):
-        if not self.container.exists:
-            logger.error(f"Container '{self.container.name}' is missing! Build with 'lammps_test build_container {self.container.name}' first!")
-            sys.exit(-1)
-        if not self.builder.exists:
-            logger.error(f"LAMMPS build '{self.builder.name}' is missing! Build with 'lammps_test build' first!")
-            sys.exit(-1)
-        print(self.lammps_build_dir)
-        print("LAMMPS Build:", self.builder.working_dir)
-        print("Workdir:", self.working_dir)
-        scripts_dir = os.path.join(self.settings.lammps_testing_dir, 'scripts')
-        run_script = os.path.join(scripts_dir, 'Run.sh')
-        self.run_command(['singularity', 'run', '-B', f'{self.settings.lammps_dir}/:{self.settings.lammps_dir}/', '-B', f'{scripts_dir}/:{scripts_dir}/', self.container.container, run_script] + args, env=self.build_env, cwd=self.working_dir, stdout=stdout)
-
-class MPIRunner(LocalRunner):
-    def __init__(self, builder, settings, nprocs=1):
-        super().__init__(builder, settings)
-        self.nprocs = nprocs
-
-    @property
-    def build_env(self):
-        env = super().build_env
-        env["LAMMPS_LAUNCHER"] = f'mpirun -np {self.nprocs} '
-        return env
-
-
-
-def get_lammps_runner(runner, builder, settings):
-    if runner == 'testing':
-        return TestRunner(builder, settings)
-    elif runner == 'local':
-        return LocalRunner(builder, settings)
-    elif runner == 'mpi':
-        return MPIRunner(builder, settings, nprocs=8)
 
 def container_build_status(value):
     return "[X]" if value else "[ ]"
@@ -161,13 +63,6 @@ def status(args, settings):
             for regtest in config.regression_tests:
                 print(" ", f"{regtest:<40}")
 
-def ensure_container_exists(container):
-    if not container.exists:
-        print(f"Missing container '{container.name}'\n")
-        print("Build container environment first!\n")
-        print(f"Usage: lammps_test buildenv --env={container.name}")
-        sys.exit(1)
-
 def checkstyle(args, settings):
     files = glob.glob(os.path.join(settings.lammps_dir, 'src', '**/*.cpp'), recursive=True)
     files.extend(glob.glob(os.path.join(settings.lammps_dir, 'src', '**/*.h'), recursive=True))
@@ -186,7 +81,6 @@ def checkstyle(args, settings):
                         print(f"{filename}:{lineno+1}:found trailing whitespaces")
         except Exception as e:
             print(f"{filename}:exception while reading file:{e}")
-
 
 def regression_test(args, settings):
     selected_config = args.config
