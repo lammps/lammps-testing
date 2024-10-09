639df3ec79cdd66c0775f83ea5556388c4265229
Axel Kohlmeyer <akohlmey@gmail.com>
2024-10-08 23:17:24 -0400

    modernize the way the wheel is build

--- scripts/common/init_testing_venv.sh
+++ scripts/common/init_testing_venv.sh
@@ -9,7 +9,7 @@ fi
 
 source pyenv/bin/activate
 
-pip install --upgrade pip setuptools wheel
+pip install --upgrade pip setuptools wheel build
 
 # avoid multiple parallel jobs writing in the same temporary directories
 PYTHON_BUILD_DIR=$PWD/python_${BUILD}
@@ -23,10 +23,8 @@ mkdir -p $PYTHON_BUILD_DIR
 # install lammps-testing package
 cd $LAMMPS_TESTING_DIR
 
-python setup.py build --build-base $PYTHON_BUILD_DIR \
-                egg_info --egg-base $PYTHON_BUILD_DIR \
-                bdist_wheel -d $PYTHON_BUILD_DIR/dist || exit 1
-
-pip install $PYTHON_BUILD_DIR/dist/lammps_testing-*.whl
+# modern way of building the wheel
+python -m build -w -v --outdir $PYTHON_BUILD_DIR
+pip install $PYTHON_BUILD_DIR/lammps_testing-*.whl
 
 deactivate
